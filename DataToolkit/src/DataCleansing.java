bb7b9bf1dbb48b2915d6758f4f301e816af58944
HeyAaronHere <aaron.chua@hotmail.com>
2020-02-13 14:06:25 +0800

    Revert "Revert "test DataCleansing""
    This reverts commit f28626e6f076a515b364ed6f835c6ead4141b60a.

--- DataToolkit/src/DataCleansing.java
+++ DataToolkit/src/DataCleansing.java
@@ -1,34 +1,72 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import org.json.JSONArray;
 import org.json.JSONObject;
 
 public class DataCleansing {
- public static JSONObject dataCleanse(JSONObject scrapeData) {
-	//remove multiple spaces
-	JSONArray extractedData =  scrapeData.getJSONArray("extracted_posts");
-	
-	for (int i = 0; i < extractedData.length(); i++ ) {
-		StringBuilder postCaption= new StringBuilder(extractedData.getJSONObject(i).getString("caption")) ;
-		boolean spaceReached = false; 
-		for (int j = postCaption.length() -1; j>-1; j-- ) {
-			if (postCaption.charAt(j) == ' ') {
-				if (spaceReached == true) {
-					postCaption.deleteCharAt(j);
-				}	
+	public static JSONObject dataCleanse(JSONObject scrapeData) {
+
+		JSONArray extractTwitterData =  scrapeData.getJSONArray("extracted_posts");
+
+		for (int i = 0; i < extractTwitterData.length(); i++ ) {
+			StringBuilder postCaption= new StringBuilder(extractTwitterData.getJSONObject(i).getString("caption")) ;
+			boolean spaceReached = false; 
+			//remove unnecessary character
+			Pattern p = Pattern.compile("[^a-zA-Z0-9!@#$%^&*()_+\\-=\\[\\]{};':\"\\|,.<>\\/?]");
+			Matcher m = p.matcher(postCaption);
+			m.replaceAll(" ");
+			//remove multiple spaces
+			for (int j = postCaption.length() -1; j >-1; j-- ) {
+				if (postCaption.charAt(j) == ' ') {
+					if (spaceReached == true) {
+						postCaption.deleteCharAt(j);
+					}	
+					else {
+						spaceReached = false;
+					}
+				}
 				else {
 					spaceReached = false;
 				}
+
 			}
-			else {
-				spaceReached = false;
-			}
-		
+			//		remove unnecessary character (test)
+			//	    Pattern p = Pattern.compile("[^a-zA-Z0-9!@#$%^&*()_+\\-=\\[\\]{};':\"\\|,.<>\\/?]");
+			//      Matcher m = p.matcher(postCaption);
+			//      System.out.println(m.replaceAll(" "));
+
+			extractTwitterData.getJSONObject(i).put("caption", postCaption.toString());
 		}
-		extractedData.getJSONObject(i).put("caption", postCaption.toString());
+
+		JSONArray extractInstagramData =  scrapeData.getJSONArray("posts");
+
+		for (int i = 0; i < extractInstagramData.length(); i++ ) {
+			StringBuilder postCaption= new StringBuilder(extractInstagramData.getJSONObject(i).getString("caption")) ;
+			boolean spaceReached = false; 
+			//remove unnecessary character
+			Pattern p = Pattern.compile("[^a-zA-Z0-9!@#$%^&*()_+\\-=\\[\\]{};':\"\\|,.<>\\/?]");
+			Matcher m = p.matcher(postCaption);
+			m.replaceAll(" ");
+			for (int j = postCaption.length() -1; j >-1; j-- ) {
+				if (postCaption.charAt(j) == ' ') {
+					if (spaceReached == true) {
+						postCaption.deleteCharAt(j);
+					}	
+					else {
+						spaceReached = false;
+					}
+				}
+				else {
+					spaceReached = false;
+				}
+
+			}
+
+			extractInstagramData.getJSONObject(i).put("caption", postCaption.toString());
+
+			
+		}
+		return scrapeData;
 	}
-	
-	//remove unecessary character
-	 
-	 return scrapeData;
- }
- 
 }
